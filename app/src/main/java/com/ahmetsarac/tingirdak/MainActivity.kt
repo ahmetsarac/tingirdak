29ced452ce9afc8c10cf3857420515516ecf39b7
Ahmet Saraç <ahmet.src.99@gmail.com>
2021-11-08 16:15:07 +0100

    HomePage is created, Colors and TopAppBar is added.

--- app/src/main/java/com/ahmetsarac/tingirdak/MainActivity.kt
+++ app/src/main/java/com/ahmetsarac/tingirdak/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ahmetsarac.tingirdak
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,31 +9,23 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.ahmetsarac.tingirdak.composables.HomePage
+import com.ahmetsarac.tingirdak.ui.theme.DarkBlue
 import com.ahmetsarac.tingirdak.ui.theme.TingirdakTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val windows = this.window
+        windows.statusBarColor = Color.argb(255, 0, 13, 107)
         setContent {
             TingirdakTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                   HomePage()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TingirdakTheme {
-        Greeting("Android")
     }
 }
